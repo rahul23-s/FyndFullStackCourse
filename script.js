da428d8672668a488baa41fe814dbd27fc7a3d24
Rahul Sharma <rahul23shrm@gmail.com>
2022-02-09 18:50:28 +0530

    Javascript First

---
+++ script.js
@@ -0,0 +1,17 @@
+var my_name = "Rahul Sharma";
+var my_boolean = true;
+var my_number = 23;
+var my_char;
+var my_float = 2.3;
+var my_symbol = Symbol("This is a Symbol");
+var my_test = null;
+var myNAN = NaN;
+
+console.log(typeof my_name);
+console.log(typeof my_boolean);
+console.log(typeof my_number);
+console.log(typeof my_char);
+console.log(typeof my_float);
+console.log(typeof my_symbol);
+console.log(typeof my_test);
+console.log(typeof myNAN);
