09784cdee22d9e234d528ee604b1d97ed5875da9
Rahul Sharma <rahul23shrm@gmail.com>
2022-02-28 20:36:48 +0530

    Node Lecture

---
+++ Node_Basics_1/index.js
@@ -0,0 +1,10 @@
+// console.log("This is coming from the nodeJs which is a javascript runtime!");
+// const http = require("http");
+
+// http
+//   .createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.write("This is coming from the server we created!");
+//     res.end();
+//   })
+//   .listen(8080);
